28dfd821d146dd605c13bffd272c890fa63de3d4
ahmadhasankhan <er.ahmad.hassan@gmail.com>
2014-11-22 13:04:05 +0545

    first commit

---
+++ app/assets/javascripts/angular/resources.js
@@ -0,0 +1,17 @@
+<!-- app/assets/javascripts/angular/resources.js -->
+
+//Factory
+myApp.factory('Products', ['$resource', function ($resource) {
+    return $resource('/products.json', {}, {
+        query: { method: 'GET', isArray: true },
+        create: { method: 'POST' }
+    })
+}]);
+
+myApp.factory('Product', ['$resource', function ($resource) {
+    return $resource('/products/:id.json', {}, {
+        show: { method: 'GET' },
+        update: { method: 'PUT', params: {id: '@id'} },
+        delete: { method: 'DELETE', params: {id: '@id'} }
+    });
+}]);
